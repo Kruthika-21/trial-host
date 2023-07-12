38aaf2b9f0f56e895b8f47bdb232d8b7f9dff85c
Jagdish Pinnada <pinnadakruthika001@gmail.com>
2023-07-12 19:40:51 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def change_page():
+    return render_template('simplepage.html')
+
+
+@app.route('/change_trip', methods=['GET', 'POST'])
+def change_trip():
+    return render_template('simp.html')
